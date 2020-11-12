05a50f73e111025a93bc8fc2eff72d7c7d4f518a
Adrian Valenzuela <avalenzuela85@gmail.com>
2020-11-12 01:38:04 -0800

    Can use Vue components in Liquid layouts

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,7 +1,6 @@
 import "../styles/index.scss"
 import Swup from "swup"
 import Vue from 'vue'
-import Hello from '../components/Hello.vue'
 import HeaderNav from '../components/HeaderNav.vue'
 
 const swup = new Swup()
