d553d85dff99c3a0b51603fe520626e8bd87d3c4
Adrian Valenzuela <avalenzuela85@gmail.com>
2020-11-15 01:50:50 -0800

    removed swup, fcking pissed me off

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,21 +1,13 @@
 import "../styles/index.scss"
-import Swup from "swup"
 import Vue from 'vue'
 import HeaderNav from '../components/HeaderNav.vue'
 
-const swup = new Swup()
-
-new Vue({
-  el: '#header-nav',
-  template: '<HeaderNav/>',
-  components: { HeaderNav }
+const headerNav = new Vue({
+    el: "#header-nav",
+    template: "<HeaderNav/>",
+    components: { HeaderNav },
 })
 
-document.addEventListener('swup:contentReplaced', (event) => {
-  const navList = document.querySelector("#nav-list");
-  navList.classList.add("hidden")
-  document.getElementById("nav-button").click() // to toggle hamburger icon back to x icon
-});
 
 console.info("Bridgetown is loaded!")
 
