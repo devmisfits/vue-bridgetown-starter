93601d4f43ae576543c54cbb1d479554e0ec7775
Adrian Valenzuela <avalenzuela85@gmail.com>
2020-11-15 19:56:48 -0800

    updated site

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,12 +1,15 @@
 import "../styles/index.scss"
 import Vue from 'vue'
 import HeaderNav from '../components/HeaderNav.vue'
+import Swup from 'swup'
 
-const headerNav = new Vue({
+var vue1 = new Vue({
     el: "#header-nav",
     template: "<HeaderNav/>",
     components: { HeaderNav },
 })
+
+const swup = new Swup()
 
 
 console.info("Bridgetown is loaded!")
